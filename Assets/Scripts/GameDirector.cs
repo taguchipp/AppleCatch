e7e8e82e341d47683986985fcbd9c4ee793609ee
ttaguchi <ozaki6626pyahoo.co.jp>
2024-03-12 11:05:01 +0900

    完成

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -32,13 +32,13 @@ public class GameDirector : MonoBehaviour
        if(this.time < 0){
         this.time = 0;
         this.generator.GetComponent<ItemGenerator>().SetParameter(10000.0f,0,0);
-       }else if(0 <= this.time && this.time < 5){
-        this.generator.GetComponent<ItemGenerator>().SetParameter(0.9f,-0.04f,3);
-       }else if(5 <= this.time && this.time < 10){
-        this.generator.GetComponent<ItemGenerator>().SetParameter(0.4f,-0.06f,6);
-       }else if(10 <= this.time && this.time < 20){
-        this.generator.GetComponent<ItemGenerator>().SetParameter(0.7f,-0.04f,4);
-       }else if(20 <= this.time && this.time < 30){
+       }else if(0 <= this.time && this.time < 4){
+        this.generator.GetComponent<ItemGenerator>().SetParameter(0.3f,-0.06f,0);
+       }else if(4 <= this.time && this.time < 12){
+        this.generator.GetComponent<ItemGenerator>().SetParameter(0.5f,-0.05f,6);
+       }else if(12 <= this.time && this.time < 23){
+        this.generator.GetComponent<ItemGenerator>().SetParameter(0.8f,-0.04f,4);
+       }else if(23 <= this.time && this.time < 30){
         this.generator.GetComponent<ItemGenerator>().SetParameter(1.0f,-0.03f,2);
        }
        this.timerText.GetComponent<TextMeshProUGUI>().text =
